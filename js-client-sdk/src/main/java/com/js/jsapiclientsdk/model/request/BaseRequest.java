fffac723ebb7f5323ed6f524cbf6834813678366
sakisaki <189444236@qq.com>
2025-01-04 11:25:30 +0800

    修改request中客户端检查

--- js-client-sdk/src/main/java/com/js/jsapiclientsdk/model/request/BaseRequest.java
+++ js-client-sdk/src/main/java/com/js/jsapiclientsdk/model/request/BaseRequest.java
@@ -2,6 +2,7 @@ package com.js.jsapiclientsdk.model.request;
 
 import cn.hutool.json.JSONUtil;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.js.jsapiclientsdk.model.response.ResultResponse;
@@ -42,8 +43,17 @@ public abstract class BaseRequest<O, T extends ResultResponse> {
         return requestParams;
     }
 
+    /**
+     * 把传入的requestParams首先转为JSON字符串，再反序列化成Map,即准确地反解析成 Map<String, Object> 类型的对象
+     * @param requestParams
+     */
     public void setRequestParams(O requestParams) {
         this.requestParams = new Gson().fromJson(JSONUtil.toJsonStr(requestParams), new TypeToken<Map<String, Object>>() {
         }.getType());
     }
+
+    // @JsonAnySetter
+    // public void setRequestParams(String key, Object value) {
+    //     requestParams.put(key, value);
+    // }
 }
