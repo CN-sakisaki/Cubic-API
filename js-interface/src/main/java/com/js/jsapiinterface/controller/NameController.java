dfda773218e7c64b6c77c1c017dd6c671158a6e5
sakisaki <189444236@qq.com>
2025-01-11 17:52:09 +0800

    注释、重构  目录和类

---
+++ js-interface/src/main/java/com/js/jsapiinterface/controller/NameController.java
@@ -0,0 +1,17 @@
+// package com.js.jsapiinterface.controller;
+//
+// import org.springframework.web.bind.annotation.*;
+//
+// import javax.servlet.http.HttpServletRequest;
+//
+// /**
+//  * @author JianShang
+//  * @version 1.0.0
+//  * @description 名称 API
+//  * @date 2024-09-13 10:33:47
+//  */
+// @RestController
+// @RequestMapping("/")
+// public class NameController {
+//
+// }
