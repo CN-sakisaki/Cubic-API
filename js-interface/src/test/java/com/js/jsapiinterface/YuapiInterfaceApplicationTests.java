3c9d25968627e242e6443e5df1941a09d78373da
sakisaki <189444236@qq.com>
2025-01-12 23:52:42 +0800

    格式化代码、移除MyBatis、MySQL依赖

--- js-interface/src/test/java/com/js/jsapiinterface/YuapiInterfaceApplicationTests.java
+++ js-interface/src/test/java/com/js/jsapiinterface/YuapiInterfaceApplicationTests.java
@@ -1,11 +1,7 @@
 package com.js.jsapiinterface;
 
-import com.yupi.yuapiclientsdk.client.YuApiClient;
-import com.yupi.yuapiclientsdk.model.User;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.annotation.Resource;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * 测试类
@@ -14,18 +10,4 @@ import javax.annotation.Resource;
  */
 @SpringBootTest
 class YuapiInterfaceApplicationTests {
-
-    @Resource
-    private YuApiClient yuApiClient;
-
-    @Test
-    void contextLoads() {
-        String result = yuApiClient.getNameByGet("yupi");
-        User user = new User();
-        user.setUsername("作_者 【程序员_鱼皮】 https://space.bilibili.com/12890453/");
-        String usernameByPost = yuApiClient.getUserNameByPost(user);
-        System.out.println(result);
-        System.out.println(usernameByPost);
-    }
-
 }
