f7f7d7867cfe0131074c88081202fec4ed92d72b
sakisaki <189444236@qq.com>
2025-02-05 16:29:38 +0800

    新增接口参数

--- js-backend/sql/create_table.sql
+++ js-backend/sql/create_table.sql
@@ -32,8 +32,6 @@ create table user
 
 
 
-
-
 create table monthly_sign_records
 (
     id                  bigint auto_increment comment '主键Id'
@@ -49,24 +47,28 @@ create table monthly_sign_records
 
 
 
-
-
 create table interface_info
 (
     id             bigint auto_increment comment '主键'
         primary key,
-    name           varchar(256)                       not null comment '名称',
-    description    varchar(256)                       null comment '描述',
-    url            varchar(512)                       not null comment '接口地址',
-    requestParams  text                               null comment '请求参数',
-    requestHeader  text                               null comment '请求头',
-    responseHeader text                               null comment '响应头',
-    status         int      default 0                 not null comment '接口状态 （0-关闭， 1-开启）',
-    method         varchar(256)                       not null comment '请求类型',
-    userId         bigint                             not null comment '创建人',
-    createTime     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime     datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDeleted      tinyint  default 0                 not null comment '是否删除(0-未删, 1-已删)'
+    name           varchar(256)                           not null comment '名称',
+    description    varchar(256)                           null comment '描述',
+    url            varchar(512)                           not null comment '接口地址',
+    requestParams  text                                   null comment '请求参数',
+    responseParams text                                   null comment '响应参数',
+    returnFormat   varchar(512) default 'JSON'            null comment '返回格式(JSON等等)',
+    requestExample text                                   null comment '请求示例',
+    requestHeader  text                                   null comment '请求头',
+    responseHeader text                                   null comment '响应头',
+    status         int          default 1                 not null comment '接口状态 （0-关闭， 1-开启）',
+    method         varchar(256)                           not null comment '请求类型',
+    userId         bigint                                 not null comment '创建人',
+    avatarUrl      varchar(1024)                          null comment '接口头像',
+    totalInvokes   bigint       default 0                 not null comment '接口总调用次数',
+    reduceScore    bigint       default 0                 not null comment '扣除积分数',
+    createTime     datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime     datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDeleted      tinyint      default 0                 not null comment '是否删除(0-未删, 1-已删)'
 )
     comment '接口信息';
 
